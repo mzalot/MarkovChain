a3b0076bed7704689d8b198cdaa456a0edd25091
mzalot <69329263+mzalot@users.noreply.github.com>
2022-03-15 09:06:17 -0600

    I added a dictionary class, set up the header, instance data, default constructor, and the file reader

---
+++ src/Dictionary.java
@@ -0,0 +1,72 @@
+/**
+ Dictionary
+ The program will create a Dictionary
+ Author: Mareks Zeile
+ Collaborator(s):
+ Collaboration:
+ Date: 2/14/22
+ On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: MZ
+ **/
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Dictionary <T, E>{
+    //instance data
+    //arraylists for the key and data
+    private ArrayList<T> values;
+    private ArrayList<E> keys;
+
+    //default constructor
+    public Dictionary(){
+        values = new ArrayList<>();
+        keys = new ArrayList<>();
+    }
+
+    //put method
+    public void put(E key, T value){
+        keys.add(key);
+        values.add(value);
+    }
+
+    //get method
+    public T get(E key){
+        int indexOfKey = keys.indexOf(key);
+        return values.get(indexOfKey);
+    }
+
+    //remove method
+    public T remove(E key){
+        int indexOfKey = keys.indexOf(key);
+        keys.remove(indexOfKey);
+        return values.remove(indexOfKey);
+    }
+
+    //contains method
+    public boolean contains(E key){
+        int indexOfKey = keys.indexOf(key);
+        return indexOfKey >= 0;
+    }
+
+    //isEmpty method
+    public boolean isEmpty(){
+        return !(values.size() > 0);
+    }
+
+    //size method
+    public int size(){
+        return values.size();
+    }
+    //return a collection of keys
+    public Collection<E> keys(){
+        Collection collection = keys;
+        return collection;
+    }
+
+    //return a collection of values
+    public Collection<E> values(){
+        Collection collection = values;
+        return collection;
+    }
+}
+
--- src/MarkovChain.java
+++ src/MarkovChain.java
@@ -1,2 +1,57 @@
+/**
+ MarkovChain
+ The program will create a MarkovChain
+ Author: Mareks Zeile
+ Collaborator(s):
+ Collaboration:
+ Date: 3/15/22
+ On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: MZ
+ **/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Random;
+
 public class MarkovChain {
+    //instance data
+    private Scanner s;
+    private Dictionary dict;
+    private File file;
+    private String storeFileName;
+    private Random r;
+    private String words;
+
+    //default constructor
+    //ask for file
+    public MarkovChain(){
+        s = new Scanner(System.in);
+        System.out.println("Enter file name: ");
+        storeFileName = s.nextLine();
+        dict = new Dictionary();
+        file = new File(storeFileName);
+        r = new Random();
+        words = "";
+
+    }
+
+    //read the text
+    public boolean readText(){
+        //did it work
+        boolean didWork = true;
+        try {
+            //scan file
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                //space to separate the words
+                words += " ";
+                words += fileReader.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            didWork = false;
+        }
+        return didWork;
+    }
 }
